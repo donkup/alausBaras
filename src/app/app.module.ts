be71adf908241d709c872bbc129f8585709e8b2b
Donatas <donkuprys@gmail.com>
2021-10-28 20:42:22 +0300

    add item list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ItemComponent } from './components/item/item.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    ItemComponent
+    ItemComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
