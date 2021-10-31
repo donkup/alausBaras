130b2c32dee2d971bb06d761f21240bde956d5ea
Donatas <donkuprys@gmail.com>
2021-10-31 18:38:06 +0200

    scss add

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -37,6 +37,31 @@ foodDark=["Zuvimi", "Saslykai"];
                            this.foodDark,
                            4, 4.2));         
   
+  this.items.push(new Alus( 1, 'Baltas Melas',
+                           'labai skanus, lengvas',
+                           '/assets/img/bm.png',
+                           ["Zuvimi", "Saslykai"],
+                           5.2, 4.2));
+   this.items.push(new Alus( 2, 'Niaukas',
+                           'labai tamsus ',
+                           '/assets/img/niau.png',
+                           this.foodDark,
+                           4, 4.2));                          
+  this.items.push(new Alus( 3, 'Dvi Sostines',
+                           'labai skanus ',
+                           '/assets/img/dvi.png',
+                           ["Zuvimi", "Kepiniai"],
+                           6, 4.2));
+  this.items.push(new Alus( 4, 'Kalifornikacija',
+                           'labai geras ',
+                           '/assets/img/kal.png',
+                           ["Zuvimi", "Saslykai"], 4.5, 4.2));
+   this.items.push(new Alus( 5, 'Alutis',
+                           'labai tamsus ',
+                           '/assets/img/niau.png',
+                           this.foodDark,
+                           4, 4.2));         
+  
   }
   public getItems(){
     return this.items;
